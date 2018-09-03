9eeec698a9d8d83a2c20a85f624acf74beedda42
MBorne <mborne@users.noreply.github.com>
2018-09-03 08:51:51 +0200

    use marked

--- src/MarkdownRenderer.js
+++ src/MarkdownRenderer.js
@@ -1,18 +1,10 @@
-const fs  = require('fs');
+const fs = require('fs');
 const relative = require('path').relative;
 const toc = require('markdown-toc');
+const marked = require('marked');
 
 const handlebars = require('handlebars');
-handlebars.registerHelper('asset',require('./helpers/asset'));
-
-/* create converter */
-var showdown    = require('showdown'),
-    converter   = new showdown.Converter({
-        simplifiedAutoLink: true,
-        tables: true,
-        ghCompatibleHeaderId: false
-    })
-;
+handlebars.registerHelper('asset', require('./helpers/asset'));
 
 /**
  * Helper class to render markdown files in a directory
@@ -23,7 +15,7 @@ class MarkdownRenderer {
      * @param {string} rootDir 
      * @param {string} layoutPath path to the template layout 
      */
-    constructor(rootDir,layoutPath){
+    constructor(rootDir, layoutPath) {
         this.rootDir = rootDir;
         this.layoutPath = layoutPath;
     }
@@ -33,21 +25,35 @@ class MarkdownRenderer {
      * @param {string} path 
      * @param {Object} options 
      */
-    renderFile(path, options){
+    renderFile(path, options) {
         /* read source and render toc based on markdown content */
-        var text        = fs.readFileSync(path,'utf8'),
-            text        = text.replace('[[toc]]',this.renderToc(text))
-        ;
+        var text = fs.readFileSync(path, 'utf8'),
+            text = text.replace('[[toc]]', this.renderToc(text))
+            ;
+
+        var renderer = new marked.Renderer();
+        renderer.heading = function (text, level) {
+            var slug = text.toLowerCase().replace(/[^\w]+/g, '-');
+//          toc.push({
+//              level: level,
+//              slug: slug,
+//              title: text
+//          });
+            return "<h" + level + " id=\"" + slug + "\"><a href=\"#" + slug + "\" class=\"anchor\"></a>" + text + "</h" + level + ">";
+        };
+
         /* render markdown */
-        var content = converter.makeHtml(text);
+        var content = marked(text, {
+            renderer: renderer
+        });
 
         /* inject result in a template */
         handlebars.registerPartial('content', content);
-        var templateSource = fs.readFileSync(this.layoutPath+'/page.html', "utf8");
+        var templateSource = fs.readFileSync(this.layoutPath + '/page.html', "utf8");
         var template = handlebars.compile(templateSource);
 
         var context = {
-            title: relative(this.rootDir,path),
+            title: relative(this.rootDir, path),
             content: content,
             rootDir: this.rootDir,
             path: path
@@ -60,14 +66,12 @@ class MarkdownRenderer {
      * Render TOC according to markdown
      * @param {string} text markdown source
      */
-    renderToc(text){
-        return toc(text,{
-            firsth1: true,
-            //stripHeadingTags: false,
-            slugify: function(text){
-                // see showdown subparser/headers with options.ghCompatibleHeaderId
+    renderToc(text) {
+        return toc(text, {
+            firsth1: false,
+            slugify: function (text) {
                 return text
-                    .replace(/[^\w]/g, '')
+                    .replace(/[^\w]+/g, '-')
                     .toLowerCase();
                 ;
             }
