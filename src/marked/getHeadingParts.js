5b35b15e766696b16e7f4ceb0c8ff77f9b4c3cc9
mborne <mborne@users.noreply.github.com>
2022-10-01 10:58:57 +0200

    refactor marked customization... (refs #12)

---
+++ src/marked/getHeadingParts.js
@@ -0,0 +1,27 @@
+const slugify = require('./slugify');
+
+const headingIdRegex = /(?: +|^)\{#([a-z][\w-]*)\}(?: +|$)/i;
+
+/**
+ * Get title and id from heading text.
+ * 
+ * Adapted from https://github.com/markedjs/marked-custom-heading-id
+ *
+ * @param {string} text
+ * @return {object} 
+ */
+function getHeadingParts(text){
+    const hasId = text.match(headingIdRegex);
+    if (!hasId) {
+        return {
+            id: slugify(text),
+            title: text
+        };
+    }
+    return {
+        id: hasId[1],
+        title: text.replace(headingIdRegex, '')
+    };
+}
+
+module.exports = getHeadingParts;
