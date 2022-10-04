ccfe59cb78f333a65f1f784e9dfaaa383260bf84
mborne <mborne@users.noreply.github.com>
2022-10-04 08:42:28 +0200

    Simplify heading generation removing anchor tag (refs #22)

--- test/mardown/render.js
+++ test/mardown/render.js
@@ -5,22 +5,19 @@ const render = require('../../src/markdown/render');
 describe('Test markdown.render', function () {
     it('should support custom id', function () {
         const result = render('# A title with a custom id {#my-id}');
-        const expected =
-            '<h1 id="my-id"><a href="#my-id" class="anchor"></a>A title with a custom id</h1>';
+        const expected = '<h1 id="my-id">A title with a custom id</h1>';
         expect(result).to.equals(expected);
     });
 
     it('should support accented letters in heading', function () {
         const result = render('# Déjà vu');
-        const expected =
-            '<h1 id="déjà-vu"><a href="#déjà-vu" class="anchor"></a>Déjà vu</h1>';
+        const expected = '<h1 id="déjà-vu">Déjà vu</h1>';
         expect(result).to.equals(expected);
     });
 
     it('should not face problem with apostrophe', function () {
         const result = render("# Let's rock!");
-        const expected =
-            '<h1 id="lets-rock"><a href="#lets-rock" class="anchor"></a>Let&#39;s rock!</h1>';
+        const expected = '<h1 id="lets-rock">Let&#39;s rock!</h1>';
         expect(result).to.equals(expected);
     });
 });
