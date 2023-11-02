989c715987ee18922ea0b04a6c47083d36756e43
mborne <mborne@users.noreply.github.com>
2023-11-02 08:59:05 +0100

    feat(filter): ignore node_modules by default (refs #37)

--- src/SourceDirFilter.js
+++ src/SourceDirFilter.js
@@ -2,6 +2,10 @@
  * Test if a file is ignored (allows to ignore some directories like .git, node_modules,...)
  */
 class SourceDirFilter {
+    constructor() {
+        this.ignoredList = [/^\.git$/, /^node_modules$/];
+    }
+
     /**
      * Test if a file is ignored
      *
@@ -10,12 +14,15 @@ class SourceDirFilter {
      * @returns {boolean}
      */
     isIgnored(relativePath) {
-        /* skip git */
-        if (relativePath.match(/\.git/)) {
-            return true;
-        } else {
-            return false;
+        const parts = relativePath.split('/');
+        for (const part of parts) {
+            for (const ignoredRegex of this.ignoredList) {
+                if (part.match(ignoredRegex)) {
+                    return true;
+                }
+            }
         }
+        return false;
     }
 }
 
--- test/SourceDirFilter.js
+++ test/SourceDirFilter.js
@@ -6,9 +6,30 @@ const defaultFilter = new SourceDirFilter();
 
 describe('test SourceDirFilter', function () {
     describe('test isIgnored with default SourceDirFilter', function () {
-        it('should ignore .git directories', function () {
+        it('should ignore .git directory', function () {
             expect(defaultFilter.isIgnored('something/.git/something-else.md'))
                 .to.be.true;
+        });
+
+        it('should ignore node_modules directory', function () {
+            expect(defaultFilter.isIgnored('node_modules/something-else.md')).to
+                .be.true;
+        });
+
+        it('should ignore node_modules sub directory', function () {
+            expect(
+                defaultFilter.isIgnored(
+                    'something/node_modules/something-else.md'
+                )
+            ).to.be.true;
+        });
+
+        it('should not ignore node_modules_example directory', function () {
+            expect(
+                defaultFilter.isIgnored(
+                    'node_modules_example/something-else.md'
+                )
+            ).to.be.false;
         });
 
         it('should not ignore other files', function () {
