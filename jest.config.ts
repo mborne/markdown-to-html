6660e2204aaa7e166032c3fe6a727798b38dc402
mborne <mborne@users.noreply.github.com>
2024-12-06 02:04:39 +0100

    chore(jest): simplify config

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,9 @@
 
 /** @type {import('jest').Config} */
 const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts', '**/*.test.ts'],
   collectCoverage: false,
   collectCoverageFrom: [
     'src/**/*.{ts,tsx,js,jsx}'
@@ -16,71 +19,14 @@ const config = {
   ],
   coverageProvider: "v8",
 
-  // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
-
-  // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
-
-  // An array of directory names to be searched recursively up from the requiring module's location
-  moduleDirectories: [
-    "node_modules"
-  ],
-
-  // An array of file extensions your modules use
-  moduleFileExtensions: [
-    "js",
-    "mjs",
-    //"cjs",
-    //"jsx",
-    "ts",
-    //"tsx",
-    //"json",
-    //"node"
-  ],
-
-  // rootDir: undefined,
-
-  // A list of paths to directories that Jest should use to search for files in
-  roots: [
-    "src",
-    "test"
-  ],
-
-  testMatch: [
-    "**/?(*.)+(spec|test).[tj]s?(x)",
-  ],
-
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
-
-  // The regexp pattern or array of patterns that Jest uses to detect test files
-  // testRegex: [],
-
   // A map from regular expressions to paths to transformers
   transform: { 
-    "^.+\\.tsx?$": ["ts-jest", { "rootDir": "." }],
-    "^.+\\.js$": ["ts-jest", { "rootDir": "./src" }] 
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.jsx?$": "ts-jest"
   },
 
-  /*
-   * An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-   * @see https://stackoverflow.com/a/49676319
-   */
-  transformIgnorePatterns: [
-    "node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"
-  ],
-
   // Indicates whether each individual test should be reported during the run
-  verbose: true,
-
+  verbose: false,
 };
 
 export default config;
